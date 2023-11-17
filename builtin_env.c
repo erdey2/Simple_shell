1775820ea971821a1d1cb7cc754398bfe384f228
Erdey <erdeysyoum@gmail.com>
2023-11-17 06:04:20 -0800

    builtin_env betty checked

--- builtin_env.c
+++ builtin_env.c
@@ -2,14 +2,14 @@
 
 /**
  * builtin_env - prints environment variables
- * 
+ *
  * Return: -1 on unsuccess
  */
 int builtin_env(void)
 {
 	int i = 0;
 
-	while(environ[i] != NULL)
+	while (environ[i] != NULL)
 	{
 		write(STDOUT_FILENO, environ[i], _strlen(environ[i]));
 		write(STDOUT_FILENO, "\n", 1);
