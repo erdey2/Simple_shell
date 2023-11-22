5ccc7b33feff09c9cc4f26dd3def05f676a7cec3
Erdey <erdeysyoum@gmail.com>
2023-11-22 03:10:46 -0800

    builtin_shell.c modified

--- builtin_shell.c
+++ builtin_shell.c
@@ -10,15 +10,14 @@ void buitin_shell(void)
 {
 	char *line;
 	char **args;
-	int status = -1;
 
-	do {
+	while (1)
+	{
 		line = get_stream();
 		args = _strtok(line);
-		status = exec_args(args);
+		exec_args(args);
+	
 		free(line);
 		free(args);
-		if (status >= 0)
-			exit(status);
-	} while (status == -1);
+	}
 }
